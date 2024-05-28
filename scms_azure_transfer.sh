6e6913f6dfc38e8afb0e8a2d70ed791aba6992f9
asif18577 <163426760+asif18577@users.noreply.github.com>
2024-05-27 19:32:35 -0500

    Create scms_azure_transfer.sh

---
+++ scms_azure_transfer.sh
@@ -0,0 +1,6 @@
+./azcopy cp "/home/scms/scms_scan_result"
+"https://onlineaccess.blob.core.window
+s.net/scms?sp=racwdli&st=2023-05-
+24T23:47:22Z&se=2023-05-25T07:47:22Z&spr=https&
+sv=2022-11-02&sr=c&sig=HHFOc4TsoVpnMzb4Zwn5DEkmI1sKoH
+v7cZo5fW2XhOQ%3D
