9496490fdb10ae49e4ba122f2e16cd5a6c6c1d73
asif18577 <163426760+asif18577@users.noreply.github.com>
2024-05-28 16:21:53 -0500

    Create nmap.sh

---
+++ nmap.sh
@@ -0,0 +1,16 @@
+/usr/bin/nmap --privilege -oN /home/scms/scms_scan.xml
+##
+#sample output ./usr/bin/nmap --privilege -oN /home/scms/scms scan.xml -O
+#192.168.200.157
+#Starting Nmap 7.80 ( https://nmap.org ) at 2023-05-22 19:19 CDT
+#Nmap scan report for 192.168.200.157
+#Host is up (0.0079s latency)
+#Not shown: 994 closed ports
+#PORT STATE SERVICE
+#22/tcp open ssh
+#143/tcp open imap
+#1720/tcp open h323q931
+#3000/tcp open ppp
+#9090/tcp open zeus-admin
+#10000/tcp open snet-sensor-mgmt
+##
